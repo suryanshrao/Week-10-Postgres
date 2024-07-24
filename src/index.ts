e5754a661cd8aae522ff636eb6bde6313d9b4cfe
Suryansh <suryanshrao01@gmail.com>
2024-07-24 23:47:05 +0530

    creating TABLE of users

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { Client} from "pg";
+import { Client } from "pg";
 
-async function getUser(email : string) {
-    const client = new Client({
-       connectionString : "postgresql://Suryansh_owner:xU9jJzW8GHFn@ep-tiny-wind-a5fr2lbt.us-east-2.aws.neon.tech/Suryansh?sslmode=require "
-    })
+const client = new Client({
+  connectionString:
+    "postgresql://Suryansh_owner:xU9jJzW8GHFn@ep-tiny-wind-a5fr2lbt.us-east-2.aws.neon.tech/Suryansh?sslmode=require ",
+});
+
+async function createUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+        CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+    console.log(result)
 }
+
+createUsersTable();
